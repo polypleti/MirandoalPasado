8715dc9808c245a74658804d2938e6fcbdbcc920
Paulette Pleticosic <p.pleticosic@gmail.com>
2022-08-02 20:21:36 -0400

    Clases creadas

---
+++ src/cl/fsj/infoclub/GolosinasMap.java
@@ -0,0 +1,17 @@
+package cl.fsj.infoclub;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class GolosinasMap {
+	
+	Map<String, Integer> golosinas = new TreeMap<>();
+	
+	golosinas.put("Chocman,100");
+	golosinas.put("Trululu,100");
+	golosinas.put("Centella,100");
+	golosinas.put("Kilate,50");
+	golosinas.put("Miti-miti, 30");
+	golosinas.put("Traga Traga,150");
+	golosinas.put("Tableton, 5");
+}
