0a580e3fa82af19fc82c45fa8e129549ed90f7a9
Paulette Pleticosic <p.pleticosic@gmail.com>
2022-08-02 22:27:11 -0400

    Clase queue mod2

--- src/cl/fsj/infoclub/JuegosQueue.java
+++ src/cl/fsj/infoclub/JuegosQueue.java
@@ -8,7 +8,6 @@ public class JuegosQueue {
 	
 	public void juegos () {
 	
-<<<<<<< HEAD
 	Queue <String> juegos = new LinkedList<>();
 	
 	juegos.add("Tombo");
@@ -19,16 +18,6 @@ public class JuegosQueue {
 	
 	System.out.println("Juegos:"+juegos);
 	System.out.println("Total: "+juegos.size());
-=======
-		Queue <String> juegos = new LinkedList<>();
-		juegos.add("Tombo");
-		juegos.add("Congelado");
-		juegos.add("Quemaditas");
-		juegos.add("Cachipun");
-		juegos.add("Pillarse");
->>>>>>> e8bc0515f93bc80beab772c070c498fa40fb33c5
-	
-		System.out.println("Juegos:"+juegos);
-		System.out.println("Total: "+juegos.size());
+
 	}
 }
