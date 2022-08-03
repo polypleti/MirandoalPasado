f40bc4dc8fed0bd793e2c3c88519836cdef845cb
QukkiQ <oscar.zapata.g@mail.pucv.cl>
2022-08-02 21:00:37 -0400

    Se agrega clase GolosinasMap

--- src/cl/fsj/infoclub/GolosinasMap.java
+++ src/cl/fsj/infoclub/GolosinasMap.java
@@ -1,17 +1,46 @@
 package cl.fsj.infoclub;
 
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class GolosinasMap {
 	
-	Map<String, Integer> golosinas = new TreeMap<>();
+	private TreeMap<String, Integer> golosinas = new TreeMap<String,Integer>();
+
+	public GolosinasMap(TreeMap<String, Integer> golosinas) {
+		this.golosinas = golosinas;
+	}
+
+	public TreeMap<String, Integer> getGolosinas() {
+		return golosinas;
+	}
+
+	public void setGolosinas(TreeMap<String, Integer> golosinas) {
+		this.golosinas = golosinas;
+	}
 	
-	golosinas.put("Chocman,100");
-	golosinas.put("Trululu,100");
-	golosinas.put("Centella,100");
-	golosinas.put("Kilate,50");
-	golosinas.put("Miti-miti, 30");
-	golosinas.put("Traga Traga,150");
-	golosinas.put("Tableton, 5");
+	
+	public void agregarGolosinas() {
+		golosinas.put("Chocman", 100);
+		golosinas.put("Trululu", 100);
+		golosinas.put("Centella", 100);
+		golosinas.put("Kilate", 50);
+		golosinas.put("Miti-Miti", 30);
+		golosinas.put("Traga Traga", 150);
+		golosinas.put("Tableton", 5);
+	}
+	 //Filtra el mapa creando uno nuevo con valores menores al que ingresa por parametro
+	public TreeMap<String,Integer> filtrarGolosinas(int maximo) {
+		TreeMap <String,Integer> mapaFiltrado = new TreeMap<String,Integer>();
+		for(Map.Entry<String,Integer> entry : golosinas.entrySet()) {
+			  String key = entry.getKey();
+			  Integer value = entry.getValue();
+			  if(value < 100)mapaFiltrado.put(key, value);
+		}
+		return mapaFiltrado;
+	}
+	
+	
 }
