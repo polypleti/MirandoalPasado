f83b1469caaf00582783a7e3518e52c664786616
Paulette Pleticosic <p.pleticosic@gmail.com>
2022-08-02 20:48:13 -0400

    Queue juegos lista

---
+++ src/cl/fsj/infoclub/InvitadosSet.java
@@ -0,0 +1,21 @@
+package cl.fsj.infoclub;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class InvitadosSet {
+
+	public void invitados () {
+		
+	 Set <String>invitados = new TreeSet <String>();
+		
+		invitados.add("Daniel");
+		invitados.add("Paola");
+		invitados.add("Facundo");
+		invitados.add("Pedro");
+		invitados.add("Jacinta");
+		invitados.add("Florencia");
+		invitados.add("Juan Pablo");
+		
+	}
+}
--- src/cl/fsj/infoclub/JuegosQueue.java
+++ src/cl/fsj/infoclub/JuegosQueue.java
@@ -1,6 +1,20 @@
 package cl.fsj.infoclub;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class JuegosQueue {
+	public void juegos () {
 	
+	Queue <String> juegos = new LinkedList<>();
+	juegos.add("Tombo");
+	juegos.add("Congelado");
+	juegos.add("Quemaditas");
+	juegos.add("Cachipun");
+	juegos.add("Pillarse");
 	
+	System.out.println("Juegos:"+juegos);
+	System.out.println("Total: "+juegos.size());
+	
+	}
 }
